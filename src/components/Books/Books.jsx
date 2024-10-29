48ea8696aa6f737a98e502f758fad041555bfcc0
tallman <vedunaik@gmail.com>
2024-10-29 12:06:51 +0530

    rendered pdf and video

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,32 +1,44 @@
 /* eslint-disable react/prop-types */
 
-import useStorage from "../../hooks/useStorage";
+import useStorage from '../../hooks/useStorage';
 
-const Books = ({bookUrl}) => {
+const Books = ({ bookUrl }) => {
     const { fileLinks } = useStorage(`Books/${bookUrl}`);
 
-  return (
-      <div className='w-[90%] mx-auto my-20'>
-          <ol>
-              {fileLinks.map((link, index) => (
-                  <li key={index} className='flex justify-start gap-8'>
-                      <a
-                          href={link}
-                          target='_blank'
-                          rel='noopener noreferrer'
-                          className=' max-w-[700px]'>
-                          {link.replace(
-                              'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2F',
-                              ''
-                          )}
-                          {''}
-                          {index + 1}
-                      </a>
-                  </li>
-              ))}
-          </ol>
-      </div>
-  );
-}
+    return (
+        <div className='w-[90%] mx-auto my-20'>
+            <ol className='flex flex-col  gap-8'>
+                {fileLinks.map((link, index) => (
+                    <li key={index} className=''>
+                        {/* <a
+                            href={link}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            className=' max-w-[700px]'>
+                            {link.replace(
+                                'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2F',
+                                ''
+                            )}
+                            {''}
+                            {index + 1}
+                        </a> */}
+                        <iframe
+                            width='300'
+                            height='700'
+                            className='transition-all
+                            duration-300 ease-in-out w-full h-[50dvh]
+                            md:h-[77dvh]'
+                            src={link}
+                            title='YouTube video player'
+                            allow='accelerometer;
+                             clipboard-write; encrypted-media;
+                            gyroscope; picture-in-picture'
+                            allowFullScreen></iframe>
+                    </li>
+                ))}
+            </ol>
+        </div>
+    );
+};
 
-export default Books
+export default Books;
