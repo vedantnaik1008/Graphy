d35746d9f9f4e9f569f927550b9a446e1d230cdb
tallman <vedunaik@gmail.com>
2024-10-29 14:31:50 +0530

    fixed tab padding issue

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -8,8 +8,8 @@ const Tabs = ({tabs}) => {
     
     
     return (
-        <section className=' overflow-y-scroll overflow-x-hidden h-screen py-20 md:py-10'>
-            <div className='transition-all duration-300 ease-in-out w-[90%] mx-auto py-5'>
+        <section className=' overflow-y-scroll overflow-x-hidden h-screen pb-28'>
+            <div className='transition-all duration-300 ease-in-out w-[90%] mx-auto'>
                 {TabsComponents.map((item) => (
                     <div className='' key={item.name}>
                         {tabs === item.name ? item.element : null}
