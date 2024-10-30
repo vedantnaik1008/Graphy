15acf7957bb92404a18738af8d7a46880f501fc0
tallman <vedunaik@gmail.com>
2024-10-30 11:47:03 +0530

    fixed not rendering pdf on mobile

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,38 +1,13 @@
 /* eslint-disable react/prop-types */
 
+import { render } from '../../data/render';
 import useStorage from '../../hooks/useStorage';
 
 const Books = ({ bookUrl }) => {
     const { fileLinks, loading } = useStorage(`Books/${bookUrl}`);
-
-    // const render = (url) => {
-    //     if (
-    //         url?.includes(
-    //             'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2FAtomic%20Habits%2Fpdf%2FBook%201-Atomic_Habits_by_James_Clear-1.pdf?alt=media&token=77263345-cdf3-4a1a-b451-ce55134e4163'
-    //         )
-    //     ) {
-    //         return 'https://drive.google.com/file/d/1jlurAicrREeP9C5PbsYdJYT9ItyuGevf/preview';
-    //     } else if (
-    //         url?.includes(
-    //             'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2FBetter%20Than%20Before%2Fpdf%2FBook%202-Better-Than-Before-Mastering-the-Habits-of-Our-Everyday-Lives.pdf?alt=media&token=f213b20d-026b-4114-b542-cb14bcd0be62'
-    //         )
-    //     ) {
-    //         return 'https://drive.google.com/file/d/1j5WmlyH9128NK7M0skXOMbQ_KzLGU4fq/preview';
-    //     } else if (url?.includes('Tiny Habits Summary .pdf')) {
-    //         return 'https://drive.google.com/file/d/1j5WmlyH9128NK7M0skXOMbQ_KzLGU4fq/preview';
-    //     } else if (url?.includes('WillPower Summary .pdf')) {
-    //         return 'https://drive.google.com/file/d/1j5WmlyH9128NK7M0skXOMbQ_KzLGU4fq/preview';
-    //     } else if (url?.includes('The Habit Blueprint summary .pdf')) {
-    //         return 'https://drive.google.com/file/d/1j5WmlyH9128NK7M0skXOMbQ_KzLGU4fq/preview';
-    //     } else if (url?.includes('You Are Not Your Brain summary .pdf')) {
-    //         return 'https://drive.google.com/file/d/1j5WmlyH9128NK7M0skXOMbQ_KzLGU4fq/preview';
-    //     } else if (url?.includes('Habits Of Highly Effective People .pdf')) {
-    //         return 'https://drive.google.com/file/d/1j5WmlyH9128NK7M0skXOMbQ_KzLGU4fq/preview';
-    //     } else if (
-    //         url?.includes('Habits Of Highly Effective People summary .pdf')
-    //     )
-    //         return url;
-    // };
+    console.log(fileLinks[0]);
+    
+    
 
 
     if (loading)
@@ -53,7 +28,7 @@ const Books = ({ bookUrl }) => {
                             className='transition-all
                             duration-300 ease-in-out w-full h-[50dvh]
                             md:h-[77dvh]'
-                            src={link}
+                            src={render(link)}
                             title='YouTube video player'
                             allow='accelerometer;
                              clipboard-write; encrypted-media;
---
+++ src/data/render.js
@@ -0,0 +1,91 @@
+export const render = (url) => {
+    if (
+        url?.includes(
+            'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2FAtomic%20Habits%2Fpdf%2FBook%201-Atomic_Habits_by_James_Clear-1.pdf?alt=media&token=77263345-cdf3-4a1a-b451-ce55134e4163'
+        )
+    ) {
+        return 'https://drive.google.com/file/d/1jlurAicrREeP9C5PbsYdJYT9ItyuGevf/preview';
+    } else if (
+        url?.includes(
+            'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2FBetter%20Than%20Before%2Fpdf%2FBook%202-Better-Than-Before-Mastering-the-Habits-of-Our-Everyday-Lives.pdf?alt=media&token=f213b20d-026b-4114-b542-cb14bcd0be62'
+        )
+    ) {
+        return 'https://drive.google.com/file/d/1-dfjbNCXj15cqa-esQkyVofq4K9Af4hO/preview';
+    } else if (
+        url?.includes(
+            'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2FTiny%20Habits%2Fpdf%2FBook%203-Tiny%20Habits%20By%20B.J.Fogg.pdf?alt=media&token=a35fc41f-1478-4c61-94fa-07cd950ee6e8'
+        )
+    ) {
+        return 'https://drive.google.com/file/d/12elWc4QOLSjPE2oeZ8QKnmQLc8XZkb_V/preview';
+    } else if (
+        url?.includes(
+            'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2FWill%20Power%2Fpdf%2FBook%204-Willpower_%20Rediscovering%20the%20Greatest%20Human%20Strength.pdf?alt=media&token=8044006c-68f3-4517-8c73-928abc196a64'
+        )
+    ) {
+        return 'https://drive.google.com/file/d/1j5WmlyH9128NK7M0skXOMbQ_KzLGU4fq/preview';
+    } else if (
+        url?.includes(
+            'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2FThe%20Habits%20Blueprint%2Fpdf%2FBook%205-The%20Habit%20Blueprint_%2015%20Simple%20Steps%20to%20Transform%20Your%20Life.pdf?alt=media&token=d73f8438-c181-4967-9a04-9133d1abfcf0'
+        )
+    ) {
+        return 'https://drive.google.com/file/d/1EbZOh2Oa5zXICWumRpxKHqn-kewQIaXh/preview';
+    } else if (
+        url?.includes(
+            'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2FYou%20Are%20Not%20Your%20Brain%2Fpdf%2FBook%206-You%20Are%20Not%20Your%20Brain_%20The%204-Step%20Solution%20for%20Changing%20Bad%20Habits%2C%20Ending%20Unhealthy%20Thinking%2C%20and%20Taking%20Control%20of%20Your%20Life.pdf?alt=media&token=4282a881-cf72-4521-a6fc-503279fe4ad1'
+        )
+    ) {
+        return 'https://drive.google.com/file/d/18xBskcfdnXViWNf4enzGyErxi6IIf6Fm/preview';
+    } else if (
+        url?.includes(
+            'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2FHabits%20Of%20Highly%20Effective%20People%2Fpdf%2FThe%207%20Habits%20Of%20Highly%20Effective%20People%20-%20Personal%20Workbook.pdf?alt=media&token=e7fb5587-7553-4bb1-8fb2-27aaa74abc88'
+        )
+    ) {
+        return 'https://drive.google.com/file/d/1sv8OgmNR-_oGv-pE_PoZfhbGQEQAb5w2/preview';
+    } else if (
+        url?.includes(
+            'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2FHabits%20Of%20Highly%20Effective%20People%2Fsummary%2Fsummary-book-the-7-habits-of-highly-effective-people-by-stephen-r-convey_63e86941.pdf?alt=media&token=2ed83747-9c35-4601-aa40-02ca66d3959f'
+        )
+    ) {
+        //hohep summary
+        return 'https://drive.google.com/file/d/1kqAr3oVhTU0bm7ddbHPiIG_z7VGMUl8G/preview';
+        //https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/
+    } else if (
+        url?.includes(
+            'Books%2FThe%20Habits%20Blueprint%2Fsummary%2Fsummary-book-the-habit-blueprint-by-patrik-edblad_63d54511.pdf?alt=media&token=611ea746-8047-438c-bec0-b0663f37b5de'
+        )
+    ) {
+        return 'https://drive.google.com/file/d/1BSOUyzpkUuwa_Np8muTE-932OmYNUxkN/preview';
+    } else if (
+        url?.includes(
+            'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2FYou%20Are%20Not%20Your%20Brain%2Fsummary%2Fsummary-book-you-are-not-your-brain-written-by-jeffrey-m-1_63df3d3a.pdf?alt=media&token=c5e53271-87de-47ff-a20d-99bf1c9736b2'
+        )
+    ) {
+        return 'https://drive.google.com/file/d/1kohZFwu5eIs_7F1Pom88vL55yrEONCbV/preview';
+    } else if (
+        url?.includes(
+            'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2FWill%20Power%2Fsummary%2FFinal%20Summary%20Book%20Willpower%20by%20Baumeister.pdf?alt=media&token=10176ab1-8397-4abf-b594-f7718e59e785'
+        )
+    ) {
+        return 'https://drive.google.com/file/d/1tMeJBn61frn-MVih5-TYDvtn8feLIYQ3/preview';
+    } else if (
+        url?.includes(
+            'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2FTiny%20Habits%2Fsummary%2FSummary%20Book%20Tiny%20Habits%20by%20Vivek%20Kumar.pdf?alt=media&token=28861850-02da-4336-a4f9-a562cbeb4c85'
+        )
+    ) {
+        return 'https://drive.google.com/file/d/1eDsytPGOHtIXMa5IwPrK9SqrbvL1kA0C/preview';
+    } else if (
+        url?.includes(
+            'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2FBetter%20Than%20Before%2Fsummary%2FBetter%20Than%20Before%20Summary%20By%20Vivek%20Kumar.pdf?alt=media&token=35b32bb2-c698-4e01-9ac8-4421b8ec1d73'
+        )
+    ) {
+        return 'https://drive.google.com/file/d/1M3AbVkK_qn0etnXBdFUtfHpn2_ptaKK4/preview';
+    } else if (
+        url?.includes(
+            'https://firebasestorage.googleapis.com/v0/b/graphy-c2078.appspot.com/o/Books%2FAtomic%20Habits%2Fsummary%2FAtomic%20Habits%20Summary%20By%20Vivek%20Kumar.pdf?alt=media&token=624c464e-4335-4db9-b6a6-63570538fdc5'
+        )
+    ) {
+        return 'https://drive.google.com/file/d/1qXIqT80WW0lVtMg_Hra4NiXlRrSshqdK/preview';
+    } else {
+        return url;
+    }
+};
