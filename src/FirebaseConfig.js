2429e8e42ce4fb549d2f7118308bbd3fc93ffeb6
tallman <vedunaik@gmail.com>
2024-10-31 09:30:39 +0530

    created .env variables

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -4,20 +4,20 @@ import { getAnalytics } from 'firebase/analytics';
 import { getStorage } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: 'AIzaSyCAb5gpgX-7QE84sbJPzutfpvhU4XFf6aI',
-    authDomain: 'graphy-c2078.firebaseapp.com',
-    databaseURL:
-        'https://graphy-c2078-default-rtdb.asia-southeast1.firebasedatabase.app/',
-    projectId: 'graphy-c2078',
-    storageBucket: 'graphy-c2078.appspot.com',
-    messagingSenderId: '722934185275',
-    appId: '1:722934185275:web:96af7e0ebeb9ea69234b1e',
-    measurementId: 'G-9YYBZ5MXDF'
+    apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+    authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_REACT_APP_DATABASE_URL,
+    projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_REACT_APP_APP_ID,
+    measurementId: import.meta.env.VITE_REACT_APP_MEASUREMENT_ID
 };
+
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
