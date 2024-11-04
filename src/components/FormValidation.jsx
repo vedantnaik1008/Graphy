d14ff4117edb42a763be5faed9d87942d004a49a
tallman <vedunaik@gmail.com>
2024-11-04 10:30:28 +0530

    +styles for input type file

--- src/components/FormValidation.jsx
+++ src/components/FormValidation.jsx
@@ -20,7 +20,7 @@ const FormValidation = () => {
                 className='absolute top-2 left-2 rounded-full px-3 py-1 z-40 text-xl border-black  border'>
                 {'<'}
             </Link>
-            <div className='px-3 py-6 md:p-10 rounded-2xl border-black border-2 bg-white text-black font-mono mx-auto h-[80dvh] w-[90%] md:w-[60%] lg:w-[40%] absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2'>
+            <div className='px-3 py-6 md:p-10 rounded-2xl border-black border-2 bg-white text-black font-mono mx-auto h-[80dvh] w-[90%] md:w-[60%] lg:w-[60%] xl:w-[40%] absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2'>
                 <form
                     onSubmit={handleSubmit}
                     className='flex flex-col gap-8 items-center justify-center h-full'>
@@ -52,7 +52,7 @@ const FormValidation = () => {
                             ref={fullBookFileRef}
                             onChange={handleFileChange}
                             accept='.pdf'
-                            className='file-input'
+                            className='custom-file-input   file-input-one'
                             placeholder='full book'
                         />
                     </label>
@@ -65,7 +65,7 @@ const FormValidation = () => {
                             ref={summaryFileRef}
                             onChange={handleFileChange}
                             accept='.pdf'
-                            className='file-input'
+                            className='custom-file-input   file-input-two'
                             placeholder='summary'
                         />
                     </label>
@@ -78,7 +78,7 @@ const FormValidation = () => {
                             ref={audioSummaryFileRef}
                             onChange={handleFileChange}
                             accept='video/*'
-                            className='file-input'
+                            className='custom-file-input   file-input-three'
                             placeholder='audio summary'
                         />
                     </label>
