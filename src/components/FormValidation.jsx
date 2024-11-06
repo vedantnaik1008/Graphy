dbbe0e41f2cf1ac4035e2462cf34e1121e9100eb
tallman <vedunaik@gmail.com>
2024-11-06 12:51:36 +0530

    +margin on y

--- src/components/FormValidation.jsx
+++ src/components/FormValidation.jsx
@@ -196,7 +196,7 @@ const FormValidation = () => {
                                                         subFolderIndex
                                                     )
                                                 }
-                                                className='bg-black text-white px-4 py-2 rounded-md'>
+                                                className='bg-black text-white px-4 py-2 my-2 rounded-md'>
                                                 Remove Subfolder
                                             </button>
                                         </div>
